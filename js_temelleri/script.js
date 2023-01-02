5928ea48a0067a5dfd78e0bcb075a5b979774af4
Huseyin Ahmetoglu <huseyinahmetoglu@artuklu.edu.tr>
2023-01-02 23:07:22 +0300

    Commit

---
+++ js_temelleri/script.js
@@ -0,0 +1,14 @@
+console.log("merhaba");
+
+var sayi1 = "10";
+let sayi2 = "20";
+
+console.log(sayi1+sayi2);
+console.log(Number(sayi1)+Number(sayi2));
+
+var sayi3 = 10;
+let sayi4 = 20;
+
+console.log(sayi1.toString()+sayi2.toString());
+
+console.log( typeof sayi1)
